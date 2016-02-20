89516bd11363d48480ec7319f09c310acc6c8532
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2016-02-20 15:51:32 +0800

    Update packages

---
+++ elpa/omtose-phellack-theme-20160212.347/omtose-phellack-theme-pkg.el
@@ -0,0 +1,6 @@
+(define-package "omtose-phellack-theme" "20160212.347" "A dark, soothing theme with a cold bluish touch."
+  '((emacs "24"))
+  :url "http:/github.com/franksn/omtose-phellack-theme/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
