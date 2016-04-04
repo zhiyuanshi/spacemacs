0f97cd20de3f0a5614c5f642f830588f51b39a83
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2016-04-04 21:33:56 +0800

    Update packages

---
+++ elpa/badwolf-theme-20160402.647/badwolf-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "badwolf-theme" "20160402.647" "Bad Wolf color theme" '((emacs "24")) :url "https://github.com/bkruczyk/badwolf-emacs" :keywords '("themes"))
---
+++ elpa/darktooth-theme-20160403.850/darktooth-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "darktooth-theme" "20160403.850" "Color theme for Emacs, when soothe and gruvbox collide" 'nil :url "http://github.com/emacsfodder/emacs-theme-darktooth")
