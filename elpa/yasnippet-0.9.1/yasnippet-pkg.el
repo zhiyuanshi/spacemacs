9a3d4845db5e21150c46a84277073cce4ad6e57d
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2016-04-11 03:37:18 +0800

    Update packages

---
+++ elpa/yasnippet-0.9.1/yasnippet-pkg.el
@@ -0,0 +1,6 @@
+(define-package "yasnippet" "0.9.1" "Yet another snippet extension for Emacs." 'nil :keywords
+  '("convenience" "emulation")
+  :url "http://github.com/capitaomorte/yasnippet")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
