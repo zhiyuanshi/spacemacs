04aed21cc0d0d7fcd3347f9badd2393e91f54578
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2016-04-19 20:45:19 +0800

    Update packages

--- elpa/color-theme-sanityinc-tomorrow-20160410.224/color-theme-sanityinc-tomorrow-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "color-theme-sanityinc-tomorrow" "20160410.224" "A version of Chris Kempson's various Tomorrow themes" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/omtose-phellack-theme-20160412.428/omtose-phellack-theme-pkg.el
@@ -0,0 +1,6 @@
+(define-package "omtose-phellack-theme" "20160412.428" "A dark, soothing theme with a cold bluish touch."
+  '((emacs "24"))
+  :url "http:/github.com/franksn/omtose-phellack-theme/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
