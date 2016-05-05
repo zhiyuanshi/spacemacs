adc715f86469722c9f5ba4769bef0cb1f4d2c55c
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2016-05-05 21:46:37 +0800

    Update packages

--- elpa/organic-green-theme-20160324.1244/organic-green-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "organic-green-theme" "20160324.1244" "Low-contrast green color theme." 'nil)
---
+++ elpa/organic-green-theme-20160504.928/organic-green-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "organic-green-theme" "20160504.928" "Low-contrast green color theme." 'nil)
