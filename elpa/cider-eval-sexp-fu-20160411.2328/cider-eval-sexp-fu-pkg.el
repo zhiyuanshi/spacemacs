4d98b299932da587191d60ea59886fecf4f86f3d
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2016-06-09 23:24:11 +0800

    Update packages

---
+++ elpa/cider-eval-sexp-fu-20160411.2328/cider-eval-sexp-fu-pkg.el
@@ -0,0 +1,1 @@
+(define-package "cider-eval-sexp-fu" "20160411.2328" "Briefly highlights an evaluated sexps." '((emacs "24") (highlight "0") (eval-sexp-fu "0.4.0")) :keywords '("languages" "clojure" "cider"))
